67d63c7260343496f7e565eb1d8d3b7fd3af6a34
Bilel BARHOUMI <infobilel@gmail.com>
2019-11-11 13:42:02 +0100

    using vuejs-boilerplate

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,vue,ts}", "!**/node_modules/**", "!**/dist/**", "!**/coverage/**"],
-
-  reporters: ["default", ["jest-junit", {"outputDirectory": 'test-results/jest', "outputName": "result.xml"}]],
-};
