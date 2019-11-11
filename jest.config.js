122da531dfc5c2f08229fdd03ad5db847410f4da
Bilel BARHOUMI <infobilel@gmail.com>
2019-11-11 10:49:06 +0100

    Install cypress

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
 
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,vue,ts}", "!**/node_modules/**", "!**/dist/**", "!**/coverage/**"]
+  collectCoverageFrom: ["src/**/*.{js,vue,ts}", "!**/node_modules/**", "!**/dist/**", "!**/coverage/**"],
+
+  reporters: ["default", "jest-junit"],
 };
